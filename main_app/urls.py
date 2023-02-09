430d32b976e544c7aa184162bc9036a6bcc8b5ca
scaev <kenkaraa@gmail.com>
2023-02-08 19:31:52 -0500

    Adds functionality for adding art

--- main_app/urls.py
+++ main_app/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('about/', views.about, name='about'),
     path('arts/', views.arts_index, name='index'),
     path('arts/<int:art_id>/', views.arts_detail, name='detail'),
+    path('arts/create/', views.ArtCreate.as_view(), name='art_create'),
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.views.generic.edit import CreateView
 from.models import Art
 
 arts = [
@@ -20,3 +21,8 @@ def arts_index(request):
 def arts_detail(request, art_id):
   art = Art.objects.get(id=art_id)
   return render(request, 'arts/detail.html', { 'art': art })
+
+
+class ArtCreate(CreateView):
+  model = Art
+  fields = '__all__'
