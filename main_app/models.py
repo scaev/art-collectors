a9541db2df300991a99c28ab88272f44ee5e24ae
scaev <kenkaraa@gmail.com>
2023-02-08 20:00:30 -0500

    Adds UpdateView & DeleteView for arts

--- main_app/models.py
+++ main_app/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.urls import reverse
 
 # Create your models here.
 class Art(models.Model):
@@ -9,3 +10,6 @@ class Art(models.Model):
 
     def __str__(self):
         return self.name
+    
+    def get_absolute_url(self):
+        return reverse('detail', kwargs={'art_id': self.id})
--- main_app/views.py
+++ main_app/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.views.generic.edit import CreateView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from.models import Art
 
 arts = [
@@ -26,3 +26,12 @@ def arts_detail(request, art_id):
 class ArtCreate(CreateView):
   model = Art
   fields = '__all__'
+
+
+class ArtUpdate(UpdateView):
+  model = Art
+  fields = ['painting_by', 'description', 'medium']
+
+class ArtDelete(DeleteView):
+  model = Art
+  success_url = '/arts'
